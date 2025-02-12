57f867324b86e01eae7c2423654fca956568cc9f
artehe <112902041+artehe@users.noreply.github.com>
2025-02-12 22:23:23 +0000

    Add chat message command and corresponding handler.

---
+++ CS2M.API/Commands/Handler/Internal/ChatMessageHandler.cs
@@ -0,0 +1,17 @@
+﻿using CS2M.API.Commands.Data.Internal;
+
+namespace CS2M.API.Commands.Handler.Internal
+{
+    public class ChatMessageHandler : CommandHandler<ChatMessageCommand>
+    {
+        public ChatMessageHandler()
+        {
+            TransactionCmd = false;
+        }
+
+        protected override void Handle(ChatMessageCommand command)
+        {
+            Chat.Instance.PrintChatMessage(command.Username, command.Message);
+        }
+    }
+}
