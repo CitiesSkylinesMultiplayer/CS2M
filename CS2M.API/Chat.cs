373a685ca95df6e721a20bafb65555be79fd608a
artehe <112902041+artehe@users.noreply.github.com>
2025-02-18 20:13:41 +0000

    Hopefully this should add styling to the panel's messaages

--- CS2M.API/Chat.cs
+++ CS2M.API/Chat.cs
@@ -15,7 +15,6 @@ namespace CS2M.API
     public interface IChat
     {
         void PrintGameMessage(string msg);
-        void PrintGameMessage(Chat.MessageType type, string msg);
         void PrintChatMessage(string username, string msg);
         string GetCurrentUsername();
     }
