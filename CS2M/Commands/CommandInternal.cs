2339093a9319ffdb2553029d1f5caa30484a199d
Nico Borgsmüller <me@kaenganxt.dev>
2025-09-14 22:37:44 +0200

    Improve packet serialization (#8)
    * Improve packet serialization
    
    - Find Command handlers correctly in all assemblies
    - Register all required types recursively
    - Support all Colossal ISerializable types using a custom serializer
    
    * Replace var with explicit type where not obvious

--- CS2M/Commands/CommandInternal.cs
+++ CS2M/Commands/CommandInternal.cs
@@ -1,17 +1,20 @@
-﻿using CS2M.API;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using CS2M.API;
 using CS2M.API.Commands;
 using CS2M.API.Networking;
 using CS2M.Helpers;
 using CS2M.Mods;
 using CS2M.Networking;
+using CS2M.Util;
 using MessagePack;
 using MessagePack.Attributeless;
 using MessagePack.Resolvers;
-using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
+using MessagePack.Unity;
+using MessagePack.Unity.Extension;
 
 namespace CS2M.Commands
 {
@@ -19,9 +22,15 @@ namespace CS2M.Commands
     {
         public static CommandInternal Instance;
 
-        private readonly ConcurrentDictionary<Type, CommandHandler> _cmdMapping = new ConcurrentDictionary<Type, CommandHandler>();
+        private readonly ConcurrentDictionary<Type, CommandHandler> _cmdMapping =
+            new ConcurrentDictionary<Type, CommandHandler>();
 
         private MessagePackSerializerOptions _model;
+
+        public CommandInternal()
+        {
+            Command.ConnectToCSM(SendToAll, SendToServer, SendToClients, GetCommandHandler);
+        }
 
         /// <summary>
         ///     This method is used to send a command to a connected client.
@@ -121,43 +130,46 @@ namespace CS2M.Commands
             _cmdMapping.Clear();
             try
             {
+                // Configure MessagePack resolver
+                IFormatterResolver resolver = CompositeResolver.Create(
+                    // enable extension packages first
+                    ColossalResolver.Instance,
+                    UnityBlitResolver.Instance,
+                    UnityResolver.Instance,
+                    StandardResolver.Instance
+                );
+                MessagePackSerializerOptionsBuilder options =
+                    MessagePackSerializerOptions.Standard.WithResolver(resolver).Configure();
                 // First, find all CSM classes, then the other mods. This is necessary to
                 // ensure that our management packets have always the same ids,
                 // as for example the ConnectionRequest and ConnectionResult commands are used
                 // to determine the installed mods.
-                IEnumerable<Type> packets = AssemblyHelper.FindClassesInCSM(typeof(CommandHandler));
+                List<Type> packets = AssemblyHelper.FindClassesInCSM(typeof(CommandHandler)).ToList();
+                var assemblies = new List<Assembly>
+                {
+                    typeof(CommandBase).Assembly,
+                    typeof(Mod).Assembly
+                };
                 foreach (ModConnection connection in ModSupport.Instance.ConnectedMods.OrderBy(mod => mod.Name))
                 {
-                    foreach (Assembly assembly in connection.CommandAssemblies.OrderBy(assembly => assembly.FullName))
-                    {
-                        packets = packets.Concat(AssemblyHelper.FindImplementationsInAssembly(assembly, typeof(CommandHandler)));
-                    }
+                    assemblies.AddRange(connection.CommandAssemblies);
+                    connection.CommandAssemblies.ForEach(input =>
+                        packets.AddRange(AssemblyHelper.FindImplementationsInAssembly(input, typeof(CommandHandler))));
                 }
 
-                Type[] handlers = packets.ToArray();
-                Log.Info($"Initializing data model with {handlers.Length} commands...");
+                options.BetterGraphOf(typeof(CommandBase), assemblies.ToArray());
 
-                // Configure MessagePack resolver
-                IFormatterResolver resolver = CompositeResolver.Create(
-                    // enable extension packages first
-                    MessagePack.Unity.Extension.UnityBlitResolver.Instance,
-                    MessagePack.Unity.UnityResolver.Instance,
-                    StandardResolver.Instance
-                );
-                var options = MessagePackSerializerOptions.Standard.WithResolver(resolver).Configure();
+                Log.Info($"Initializing data model with {packets.Count} commands...");
 
                 // Create instances of the handlers, initialize mappings and register command subclasses in the protobuf model
-                foreach (Type type in handlers)
+                foreach (Type type in packets)
                 {
-                    CommandHandler handler = (CommandHandler)Activator.CreateInstance(type);
+                    var handler = (CommandHandler)Activator.CreateInstance(type);
                     bool added = _cmdMapping.TryAdd(handler.GetDataType(), handler);
                     if (!added)
                     {
                         Log.Debug($"Handler for {handler.GetDataType()} already exists");
                     }
-
-                    // Add subtype to the MsgPack model with all attributes
-                    options.SubType(typeof(CommandBase), handler.GetDataType());
                 }
 
                 _model = options.Build();
@@ -166,11 +178,6 @@ namespace CS2M.Commands
             {
                 Log.Error("Failed to initialize data model", ex);
             }
-        }
-
-        public CommandInternal()
-        {
-            Command.ConnectToCSM(this.SendToAll, this.SendToServer, this.SendToClients, this.GetCommandHandler);
         }
     }
 }
---
+++ CS2M/Util/MessagePackExtensions.cs
@@ -0,0 +1,165 @@
+using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Colossal.Serialization.Entities;
+using MessagePack;
+using MessagePack.Attributeless;
+using MessagePack.Attributeless.Implementation;
+using MessagePack.Formatters;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace CS2M.Util
+{
+    public static class MessagePackExtensions
+    {
+        public static MessagePackSerializerOptionsBuilder BetterGraphOf(
+            this MessagePackSerializerOptionsBuilder builder, Type self, params Assembly[] assemblies)
+        {
+            var result = new List<Type>();
+            Add(self);
+
+            void Add(Type type)
+            {
+                if (result.Contains(type)) return;
+
+                if (type.IsConstructedGenericType)
+                {
+                    foreach (Type t in type.GenericTypeArguments) Add(t);
+                }
+
+                if (type.IsArray)
+                {
+                    Add(type.GetElementType());
+                    return;
+                }
+
+                if (!assemblies.Contains(type.Assembly)) return;
+
+                result.Add(type);
+                IEnumerable<Type> children =
+                    type.GetProperties()
+                        .Where(p => !p.IsIndexed())
+                        .Select(p => p.PropertyType)
+                        .Distinct()
+                        .Where(x => !x.IsEnum);
+                IEnumerable<Type> derivations = type.GetSubTypes(assemblies);
+
+                foreach (Type t in children.Concat(derivations))
+                {
+                    Add(t);
+                }
+            }
+
+            foreach (Type t in result)
+            {
+                if (t.IsAbstract) builder.AllSubTypesOf(t, assemblies);
+                else builder.AutoKeyed(t);
+            }
+
+            return builder;
+        }
+    }
+
+    public class ColossalFormatter<T> : IMessagePackFormatter<T> where T : struct, ISerializable
+    {
+        public void Serialize(
+            ref MessagePackWriter writer, T value, MessagePackSerializerOptions options)
+        {
+            // Create writer and native list for serialization
+            var wrappedWriter = new BinaryWriter();
+            var byteArray = new NativeList<byte>(0, Allocator.Temp);
+            wrappedWriter.Initialize(wrappedWriter.context, byteArray, new NativeArray<Entity>());
+
+            // Do serialization
+            value.Serialize(wrappedWriter);
+
+            // Copy result to MessagePack writer
+            writer.Write(byteArray.ToArray(Allocator.Temp).ToArray());
+
+            // Dispose native list if necessary
+            byteArray.Dispose();
+        }
+
+        public T Deserialize(
+            ref MessagePackReader reader, MessagePackSerializerOptions options)
+        {
+            options.Security.DepthStep(ref reader);
+
+            // Read byte array from MessagePack reader
+            byte[] bytes = reader.ReadBytes().GetValueOrDefault().ToArray();
+
+            // Create reader and native array for deserialization
+            var wrappedReader = new BinaryReader();
+            var byteArray = new NativeArray<byte>(bytes, Allocator.Temp);
+            var position = new NativeReference<int>(0, Allocator.Temp);
+            wrappedReader.Initialize(wrappedReader.context, byteArray, position, new NativeArray<Entity>());
+
+            // Create default target element and deserialize
+            T elem = default;
+            elem.Deserialize(wrappedReader);
+
+            // Dispose native fields if necessary
+            byteArray.Dispose();
+            position.Dispose();
+
+            reader.Depth--;
+            return elem;
+        }
+    }
+
+    public class ColossalResolver : IFormatterResolver
+    {
+        // Resolver should be singleton.
+        public static readonly IFormatterResolver Instance = new ColossalResolver();
+
+        private ColossalResolver()
+        {
+        }
+
+        // GetFormatter<T>'s get cost should be minimized so use type cache.
+        public IMessagePackFormatter<T> GetFormatter<T>()
+        {
+            return FormatterCache<T>.Formatter;
+        }
+
+        private static class FormatterCache<T>
+        {
+            public static readonly IMessagePackFormatter<T> Formatter;
+
+            // generic's static constructor should be minimized for reduce type generation size!
+            // use outer helper method.
+            static FormatterCache()
+            {
+                Formatter = (IMessagePackFormatter<T>)ColossalResolverGetFormatterHelper.GetFormatter(typeof(T));
+            }
+        }
+    }
+
+    internal static class ColossalResolverGetFormatterHelper
+    {
+        // If type is concrete type, use type-formatter map
+        private static readonly Dictionary<Type, object> FormatterMap = new Dictionary<Type, object>();
+
+        internal static object GetFormatter(Type t)
+        {
+            if (FormatterMap.TryGetValue(t, out object formatter))
+            {
+                return formatter;
+            }
+
+            if (typeof(ISerializable).IsAssignableFrom(t) &&
+                t.IsValueType) // Check type constraints for custom serializer
+            {
+                formatter = Activator.CreateInstance(typeof(ColossalFormatter<>).MakeGenericType(t));
+                FormatterMap.Add(t, formatter);
+                return formatter;
+            }
+
+            // If type can not get, must return null for fallback mechanism.
+            return null;
+        }
+    }
+}
